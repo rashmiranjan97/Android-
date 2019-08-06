c7fe7f8dcedfd3d30f3aa7fce1bb29251e3d532d
Rashmi Ranjan <40856575+rashmiranjan97@users.noreply.github.com>
2019-08-06 15:10:31 +0530

    updated task

--- MainActivity.kt
+++ MainActivity.kt
@@ -9,18 +9,28 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    internal lateinit var holder: String
+    private lateinit var holder: String
+    private lateinit var holder1: String
+    private lateinit var user :EditText
+    private lateinit var password :EditText
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        
+        user = findViewById(R.id.edittext1)
+        password = findViewById(R.id.edittext2)
+
 
         buttont.setOnClickListener() {
 
             val intent = Intent(this,Screen2::class.java)
-            holder = edittext1.text.toString()
-            intent.putExtra("holder1", holder)
+            holder = user.text.toString()
+            holder1 = password.text.toString()
+            intent.putExtra("holder", holder)
+            intent.putExtra("holder1", holder1)
             startActivity(intent)
 
         }
