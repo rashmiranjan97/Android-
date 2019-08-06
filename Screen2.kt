a9a8f0be2833a2b1f6808ec7e65c83e00f7ad8f6
Rashmi Ranjan <40856575+rashmiranjan97@users.noreply.github.com>
2019-08-06 15:11:08 +0530

    updated task

--- Screen2.kt
+++ Screen2.kt
@@ -8,16 +8,20 @@ import kotlinx.android.synthetic.main.activity2.*
 
 class Screen2 : AppCompatActivity(){
 
-    internal lateinit var superr : EditText
+    private lateinit var superr : EditText
+    private lateinit var superr1 : EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity2)
         val superr = findViewById<TextView>(R.id.edittext3)
+        val superr1 = findViewById<TextView>(R.id.edittext4)
 
-       val holder = intent.getStringExtra("holder1")
+       val holder = intent.getStringExtra("holder")
+       val holder1 = intent.getStringExtra("holder1")
         superr.text = holder
+        superr1.text = holder1
 
     }
 }
